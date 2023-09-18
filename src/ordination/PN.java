f672f5455b2b3b3118ed450a2a305a00fd3392c0
losha <losh0001@gmail.com>
2023-09-18 09:26:16 +0200

    Initial commit

--- src/ordination/PN.java
+++ src/ordination/PN.java
@@ -2,7 +2,7 @@ package ordination;
 
 import java.time.LocalDate;
 
-public class PN {
+public class PN extends Ordination {
 
     private double antalEnheder;
 
@@ -13,6 +13,11 @@ public class PN {
      * @param givesDen
      * @return
      */
+
+    public PN(double antalEnheder, ) {
+
+    }
+
     public boolean givDosis(LocalDate givesDen) {
         // TODO
         return false;   
@@ -21,6 +26,11 @@ public class PN {
     public double doegnDosis() {
         // TODO
         return 0.0;
+    }
+
+    @Override
+    public String getType() {
+        return null;
     }
 
 
