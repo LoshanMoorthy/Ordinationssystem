d23860c15151263bb81baf256fd78399b935d3ad
Jens <jensl18@student.sdu.dk>
2023-09-18 10:58:30 +0200

    Initial commit

--- src/ordination/DagligSkaev.java
+++ src/ordination/DagligSkaev.java
@@ -1,12 +1,52 @@
 package ordination;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
-public class DagligSkaev {
+public class DagligSkaev extends Ordination {
     // TODO
 
+    private ArrayList<Dosis> doser = new ArrayList<>();
+    private LocalTime tidspunkt;
+    private int mængde;
+
+
+    public DagligSkaev(LocalDate startDen, LocalDate slutDen, LocalTime tidspunkt, int mængde) {
+        super(startDen,slutDen);
+        this.tidspunkt = tidspunkt;
+        this.mængde = mængde;
+
+    }
 
     public void opretDosis(LocalTime tid, double antal) {
         // TODO
+        Dosis dosis = new Dosis(tidspunkt,mængde);
+        doser.add(dosis);
+
+    }
+
+
+
+    @Override
+    public double samletDosis() {
+        double samlet = 0;
+        for(Dosis d : doser){
+            samlet += d.getAntal();
+        }
+        return samlet;
+    }
+
+    @Override
+    public double doegnDosis() {
+        for(Dosis d : doser){
+
+        }
+        return 0;
+    }
+
+    @Override
+    public String getType() {
+        return null;
     }
 }
