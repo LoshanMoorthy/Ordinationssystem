71e735f0b01ffbd089fec129d881c1095651569e
kristinacortsen <eaa23krco@students.eaaa.dk>
2023-09-18 10:59:15 +0200

    Updated Dagligfast

--- src/ordination/DagligFast.java
+++ src/ordination/DagligFast.java
@@ -1,9 +1,16 @@
 package ordination;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class DagligFast {
-    private final Dosis[] doser = new Dosis[4];
+public class DagligFast extends Ordination{
+    private Dosis[] doser = new Dosis[4];
+
+    public DagligFast(LocalDate startDen, LocalDate slutDen, Dosis[] doser) {
+        super(startDen, slutDen);
+        this.doser = doser;
+    }
+
 
     public Dosis[] getDosis() {
         return doser;
@@ -11,7 +18,40 @@ public class DagligFast {
 
     public Dosis createDosis(LocalTime tid, double antal) {
         Dosis dosis = new Dosis(tid, antal);
-        doser.add(person);
-        return person;
+
+        return dosis;
     }
+
+    /* * Returnerer den totale dosis der er givet i den periode ordinationen er gyldig
+     *
+     * @return */
+
+    public double samletDosis() {
+        double samletDosis = 0;
+        for (int i = 0; i < doser.length; i++) {
+            samletDosis = antalDage() *
+            }
+
+
+        }
+        return samletDosis;
+    }
+
+     /* * Returnerer den gennemsnitlige dosis givet pr dag i den periode ordinationen er gyldig
+     *
+     * @return */
+
+    public double doegnDosis() {
+        return 0;
+    }
+
+
+     /* * Returnerer ordinationstypen som en String
+     *
+     * @return */
+
+    public String getType() {
+        return null;
+    }
+
 }
